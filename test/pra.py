411102fcf2aef9c9ed2de4bf983ec0a3ce668cf3
Thikaru <H.tomita1823@gmail.com>
2022-05-22 00:51:13 +0900

    My Django Girls app, first commit

---
+++ test/pra.py
@@ -0,0 +1,27 @@
+from turtle import end_fill
+
+
+print('HELLO PYTHON!!!');
+
+a=3;
+b=2;
+if a<b :
+    print('rrr');
+else :
+    print('qwe');
+
+aki = [12,34,56];
+
+for i in aki :
+    print(i);
+
+
+j="akyon"
+for i in range(5):
+    j = j+j;
+    print(len(j));
+    print(j);
+
+
+
+
